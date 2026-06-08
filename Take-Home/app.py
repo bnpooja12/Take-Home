2dcab1413f2436aaa5b10f5fe6a324db0eb16b92
bnpooja12 <bnpooja12@gmail.com>
2026-06-09 00:31:17 +0530

    Add file listing feature to app.py

--- Take-Home/app.py
+++ Take-Home/app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import os
 
+st.write("Files in directory:")
+st.write(os.listdir("."))
 # --------------------------------------------------
 # PAGE CONFIG
 # --------------------------------------------------
